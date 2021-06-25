c5a57fc02e41ecf2f3e9fbbabf327f596b1e8d01
1foreveronline1 <82825085+1foreveronline1@users.noreply.github.com>
2021-06-25 21:38:30 +0300

    Add files via upload

---
+++ Билет5/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Билет5
+{
+  class rodent // родительский класс грызунов
+  {
+    public string name;
+    public int height;
+    public int lenght;
+    public int weight;
+
+    public rodent(int height, int lenght, int weight)
+    {
+      this.height = height;
+      this.lenght = lenght;
+      this.weight = weight;
+    }
+
+    public rodent(int height)
+    {
+      this.height = height;
+    }
+  } 
+
+  class suslik : rodent
+  {
+    public suslik(int height, int lenght, int weight) : base(height, lenght, weight)
+    {
+      name = "Даурский суслик";
+    }
+
+  }
+
+  class sonya : rodent
+  {
+    public sonya(int height) : base(height)
+    {
+      name = "Мышевидная соня";
+    }
+  }
+
+  class slepish : rodent
+  {
+    public slepish(int height, int lenght, int weight) : base(height, lenght, weight)
+    {
+      name = "Белозубый слепыш";
+    }
+  }
+
+  class cat
+  {
+    private string name = "Диззи";
+    
+    public void Hunt(List<rodent> animals)
+    {
+      List<rodent> eaten = new List<rodent>();
+      List<rodent> caught = new List<rodent>();
+      List<rodent> free = new List<rodent>();
+
+      foreach (var x in animals)
+      {
+        if (x.name == "Мышевидная соня")
+        {
+          free.Add(x);
+        }
+
+        if (x.height > 6 && x.lenght >=5 && x.weight > 9)
+        {
+          caught.Add(x);
+          
+          if (x.name == "Белозубый слепыш")
+          {
+            eaten.Add(x);
+            caught.Remove(x);
+          }
+
+          if (x.name == "Даурский суслик")
+          {
+            caught.Remove(x);
+            free.Add(x);
+          }
+        }
+      }
+    }
+  }
+
+}
